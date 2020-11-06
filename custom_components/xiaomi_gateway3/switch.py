4321987d51e6e8e4272556945139dc6084474edc
hellkun <imrk@qq.com>
2020-11-06 17:09:25 +0800

    add Gateway3MeshSwitch

--- custom_components/xiaomi_gateway3/switch.py
+++ custom_components/xiaomi_gateway3/switch.py
@@ -1,9 +1,11 @@
 import logging
+import time
 
 from homeassistant.components import persistent_notification
 from homeassistant.helpers.entity import ToggleEntity
 
 from . import DOMAIN, Gateway3Device
+from .core import bluetooth
 from .core.gateway3 import Gateway3
 
 _LOGGER = logging.getLogger(__name__)
@@ -11,11 +13,13 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     def setup(gateway: Gateway3, device: dict, attr: str):
-        async_add_entities([
-            FirmwareLock(gateway, device, attr)
-            if attr == 'firmware lock' else
-            Gateway3Switch(gateway, device, attr)
-        ])
+        _LOGGER.debug(f"setup device: {device}")
+        if attr == 'firmware lock':
+            async_add_entities([FirmwareLock(gateway, device, attr)])
+        elif device['type'] == 'mesh':
+            async_add_entities([Gateway3MeshSwitch(gateway, device, attr)])
+        else:
+            async_add_entities([Gateway3Switch(gateway, device, attr)])
 
     gw: Gateway3 = hass.data[DOMAIN][config_entry.entry_id]
     gw.add_setup('switch', setup)
@@ -36,6 +40,86 @@ class Gateway3Switch(Gateway3Device, ToggleEntity):
 
     def turn_off(self):
         self.gw.send(self.device, {self._attr: 0})
+
+
+class Gateway3MeshSwitch(Gateway3Device, ToggleEntity):
+
+    _siid = 0
+    _piid = 0
+
+    async def async_added_to_hass(self):
+        await super().async_added_to_hass()
+
+        if 'childs' in self.device:
+            for did in self.device['childs']:
+                self.gw.add_update(did, self.update)
+
+    async def async_will_remove_from_hass(self) -> None:
+        await super().async_will_remove_from_hass()
+
+        if 'childs' in self.device:
+            for did in self.device['childs']:
+                self.gw.remove_update(did, self.update)
+
+    def __init__(self, gateway: Gateway3, device: dict, attr: str):
+        super(Gateway3MeshSwitch, self).__init__(gateway, device, attr)
+
+        mesh_prop = device['mesh_prop']
+        _LOGGER.debug(f"_init_ {mesh_prop}")
+
+        self._siid = mesh_prop[0]
+        self._piid = mesh_prop[1]
+
+        self._unique_id = f"{self.device['mac']}_{self._siid}_{self._piid}_{self._attr}"
+        self._name = (self.device['device_name'] + ' ' +
+                      mesh_prop[2].title())
+
+        self.entity_id = f"{DOMAIN}.{self._unique_id}"
+
+    @property
+    def should_poll(self) -> bool:
+        return True
+
+    @property
+    def is_on(self) -> bool:
+        return self._state
+
+    def update(self, data: dict = None):
+        if data is None:
+            did = (self.device['childs'][0]
+                   if 'childs' in self.device
+                   else self.device['did'])
+
+            try:
+                payload = [{'did': did,'siid': self._siid,'piid': self._piid,}]
+                resp = self.gw.miio.send('get_properties', payload)
+                _LOGGER.debug(f"{self.gw.host} | {did} resp = {resp}")
+                data = bluetooth.parse_xiaomi_mesh_raw(resp)[did]
+            except Exception as e:
+                _LOGGER.debug(f"{self.gw.host} | {did} poll error: {e}")
+                self.device['online'] = False
+                return
+
+            # _LOGGER.debug(f"{self.gw.host} | {did} poll mesh <= {data}")
+            self._update(data)
+
+        else:
+            self._update(data)
+            self.async_write_ha_state()
+
+
+    def _update(self, data: dict):
+        self.device['online'] = True
+        _LOGGER.debug(f"{self.gw.host} | {self.device['did']} _update {data}")
+        self._state = data[(self._siid, self._piid)]
+        
+    def turn_on(self):
+        self.gw.send_mesh_raw(self.device, {(self._siid, self._piid): True})
+        time.sleep(.5)  # delay before poll actual status
+
+    def turn_off(self):
+        self.gw.send_mesh_raw(self.device, {(self._siid, self._piid): False})
+        time.sleep(.5)  # delay before poll actual statu
 
 
 class FirmwareLock(Gateway3Switch):
