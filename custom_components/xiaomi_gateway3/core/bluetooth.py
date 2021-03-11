03ea55a4579e446e47a84ce327c1e56f91cf558d
Alexey Khit <alexey.khit@gmail.com>
2021-03-11 08:54:05 +0300

    Rename Qingping TH Sensor

--- custom_components/xiaomi_gateway3/core/bluetooth.py
+++ custom_components/xiaomi_gateway3/core/bluetooth.py
@@ -29,7 +29,7 @@ DEVICES = [{
     2691: ["Xiaomi", "Qingping Motion Sensor", "CGPR1"],
     # logs: https://github.com/AlexxIT/XiaomiGateway3/issues/180
     2701: ["Xiaomi", "Motion Sensor 2", "RTCGQ02LM"],  # 15,4119,4120
-    2888: ["Xiaomi", "Qingping Temp & RH Monitor", "CGG1"],
+    2888: ["Xiaomi", "Qingping TH Sensor", "CGG1"],  # same model as 839?!
 }, {
     # Mesh Light
     0: ["Xiaomi", "Mesh Group", "Mesh Group"],
