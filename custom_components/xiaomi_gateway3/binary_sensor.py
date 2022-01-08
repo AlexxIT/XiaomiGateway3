9bbf7e92f265894e0f021d5fbf520182ab4fd53c
Alexey Khit <alexey.khit@gmail.com>
2022-01-08 12:06:12 +0300

    Fix hass.async_create_task for motion sensor and buttons

--- custom_components/xiaomi_gateway3/binary_sensor.py
+++ custom_components/xiaomi_gateway3/binary_sensor.py
@@ -89,6 +89,16 @@ class XiaomiMotionSensor(XEntity, BinarySensorEntity):
         self._attr_is_on = False
         self.async_write_ha_state()
 
+    async def async_will_remove_from_hass(self):
+        if self._clear_task:
+            self._clear_task.cancel()
+
+        if self._attr_is_on:
+            self._attr_is_on = False
+            self.async_write_ha_state()
+
+        await super().async_will_remove_from_hass()
+
     @callback
     def async_set_state(self, data: dict):
         # fix 1.4.7_0115 heartbeat error (has motion in heartbeat)
@@ -126,7 +136,7 @@ class XiaomiMotionSensor(XEntity, BinarySensorEntity):
 
             self.debug(f"Extend delay: {delay} seconds")
 
-            self._clear_task = self.hass.async_create_task(
+            self._clear_task = self.hass.loop.create_task(
                 self.async_clear_state(abs(delay))
             )
 
