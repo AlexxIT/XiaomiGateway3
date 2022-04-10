04175d7530b93e1766192681e3bdfdcd5cc3296a
Alexey Khit <alexey.khit@gmail.com>
2022-04-10 09:05:59 +0300

    Fix custom units support for old Hass #723

--- custom_components/xiaomi_gateway3/core/backward.py
+++ custom_components/xiaomi_gateway3/core/backward.py
@@ -6,6 +6,8 @@ from homeassistant.helpers.entity import Entity
 # https://github.com/home-assistant/core/blob/933e0161501ffc160fb9009baf0112eabbae17f7/homeassistant/helpers/entity.py#L223-L238
 hass_version_supported = (MAJOR_VERSION, MINOR_VERSION) >= (2021, 7)
 
+# EntityCategory support from v2021.12
+# https://github.com/home-assistant/core/blob/604a2ac3270bc51f050e0f7a7ce5079bf6da5225/homeassistant/helpers/entity.py#L183
 if (MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION) >= (2021, 12):
     from homeassistant.helpers.entity import EntityCategory
 
@@ -15,6 +17,8 @@ else:
     ENTITY_CATEGORY_CONFIG = "config"
     ENTITY_CATEGORY_DIAGNOSTIC = "diagnostic"
 
+# native_value support from v2021.9
+# https://github.com/home-assistant/core/blob/245eec7041a2d856e1da435b686af34fee06493a/homeassistant/components/sensor/__init__.py#L161
 if (MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION) >= (2021, 9):
     Backward1 = Entity
 else:
@@ -27,25 +31,6 @@ else:
         def _attr_native_value(self, value):
             self._attr_state = value
 
-if (MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION) >= (2022, 3, "3"):
-    from homeassistant.helpers.entity import EntityPlatformState
-
-
-    class Backward2(Backward1):
-        @property
-        def added(self) -> bool:
-            return self._platform_state == EntityPlatformState.ADDED
-else:
-    class Backward2(Backward1):
-        @property
-        def added(self) -> bool:
-            # noinspection PyUnresolvedReferences
-            return self._added
-
-if (MAJOR_VERSION, MINOR_VERSION) >= (2022, 4):
-    XEntityBase = Backward2
-else:
-    class XEntityBase(Backward2):
         @property
         def _attr_native_unit_of_measurement(self):
             return self._attr_unit_of_measurement
@@ -53,3 +38,20 @@ else:
         @_attr_native_unit_of_measurement.setter
         def _attr_native_unit_of_measurement(self, value):
             self._attr_unit_of_measurement = value
+
+# EntityPlatformState support from v2022.3.3
+# https://github.com/home-assistant/core/blob/737c502e948ca0fb944e4732925a3b32c9761171/homeassistant/helpers/entity.py#L211
+if (MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION) >= (2022, 3, "3"):
+    from homeassistant.helpers.entity import EntityPlatformState
+
+
+    class XEntityBase(Backward1):
+        @property
+        def added(self) -> bool:
+            return self._platform_state == EntityPlatformState.ADDED
+else:
+    class XEntityBase(Backward1):
+        @property
+        def added(self) -> bool:
+            # noinspection PyUnresolvedReferences
+            return self._added
