0ff9f817383248e8abcf7971cd9b13e664b90787
Aleh Katsuba <kolegych@gmail.com>
2024-01-06 13:21:56 +0300

    Fix color and brightness support at light.py

--- custom_components/xiaomi_gateway3/light.py
+++ custom_components/xiaomi_gateway3/light.py
@@ -1,13 +1,6 @@
 import asyncio
 
-from homeassistant.components.light import (
-    ATTR_BRIGHTNESS,
-    ATTR_COLOR_TEMP,
-    ATTR_EFFECT,
-    LightEntity,
-    LightEntityFeature,
-    ATTR_TRANSITION,
-)
+from homeassistant.components.light import *
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import STATE_ON
 from homeassistant.core import callback, HomeAssistant
@@ -47,8 +40,9 @@ class XiaomiLight(XEntity, LightEntity, RestoreEntity):
 
         for conv in device.converters:
             if conv.attr == ATTR_BRIGHTNESS:
-                self._attr_supported_features |= LightEntityFeature.TRANSITION
+                self._attr_supported_features |= SUPPORT_BRIGHTNESS | SUPPORT_TRANSITION
             elif conv.attr == ATTR_COLOR_TEMP:
+                self._attr_supported_features |= SUPPORT_COLOR_TEMP                
                 if hasattr(conv, "minm") and hasattr(conv, "maxm"):
                     self._attr_min_mireds = conv.minm
                     self._attr_max_mireds = conv.maxm
