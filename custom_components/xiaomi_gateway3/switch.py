8365cca651f8e979c528928909ae2d2dff347b61
Alex X <alexey.khit@gmail.com>
2024-04-18 16:37:29 +0300

    Remove unused code

--- custom_components/xiaomi_gateway3/switch.py
+++ custom_components/xiaomi_gateway3/switch.py
@@ -10,20 +10,7 @@ async def async_setup_entry(hass, entry, async_add_entities) -> None:
     XEntity.ADD[entry.entry_id + "switch"] = async_add_entities
 
 
-# noinspection PyArgumentList
-DESCRIPTIONS = [
-    SwitchEntityDescription(
-        key="led",
-        entity_category=EntityCategory.CONFIG,
-        entity_registry_enabled_default=False,
-        icon="mdi:led-off",
-    ),
-]
-
-
 class XSwitch(XEntity, SwitchEntity, RestoreEntity):
-    DESCRIPTIONS = DESCRIPTIONS
-
     def set_state(self, data: dict):
         self._attr_is_on = bool(data[self.attr])
 
