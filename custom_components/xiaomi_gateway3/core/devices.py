beed269a1d6d6450b73a77db62aacacaba06801b
Zkdc <Zkdc2345@qq.com>
2024-12-19 23:33:32 +0800

    fix energy proportion error for XMZNCXB01QM（Mesh Power Strip 2）

--- custom_components/xiaomi_gateway3/core/devices.py
+++ custom_components/xiaomi_gateway3/core/devices.py
@@ -2733,7 +2733,7 @@ DEVICES += [{
         BaseConv("switch", "switch", mi="2.p.1"),  # bool
         BaseConv("mode", "switch", mi="2.p.2"),  # int8
         MathConv("chip_temperature", "sensor", mi="2.p.3", round=2),  # float, diagnostic
-        MathConv("energy", "sensor", mi="3.p.1", multiply=0.1, round=2),
+        MathConv("energy", "sensor", mi="3.p.1", round=2),
         MathConv("power", "sensor", mi="3.p.2", round=2),  # float
         MathConv("voltage", "sensor", mi="3.p.3", multiply=0.001, round=2),  # float
         MathConv("current", "sensor", mi="3.p.4", multiply=0.001, round=2),  # float
