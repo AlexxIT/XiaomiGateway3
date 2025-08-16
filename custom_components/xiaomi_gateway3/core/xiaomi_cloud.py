e96447d2b62feb00758508b5ec3bcfcaaa0d7e27
Alex X <alexey.khit@gmail.com>
2025-08-16 16:27:35 +0300

    Xiaomi authorization has been completely rewritten

--- custom_components/xiaomi_gateway3/core/xiaomi_cloud.py
+++ custom_components/xiaomi_gateway3/core/xiaomi_cloud.py
@@ -1,52 +1,34 @@
-"""
-The base logic was taken from project https://github.com/squachen/micloud
-
-I had to rewrite the code to work asynchronously and handle timeouts for
-requests to the cloud.
-
-MIT License
-
-Copyright (c) 2020 Sammy Svensson
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-import asyncio
 import base64
 import hashlib
 import json
-import logging
 import os
 import random
 import string
 import time
+from typing import TypedDict
 
+from Crypto.Cipher import ARC4
 from aiohttp import ClientSession
 
-_LOGGER = logging.getLogger(__package__ + ".cloud")
-
+COOKIES = ("userId", "cUserId", "serviceToken")
+SDK_VERSION = "4.2.29"
 SERVERS = ["cn", "de", "i2", "ru", "sg", "us"]
-UA = "Android-7.1.1-1.0.0-ONEPLUS A3010-136-%s APP/xiaomi.smarthome APPV/62830"
+SID = "xiaomiio"
+
+
+class AuthResult(TypedDict, total=False):
+    ok: bool
+    captcha: bytes | None
+    email: str | None
+    token: str | None
+    exception: Exception | None
 
 
 class MiCloud:
-    auth: dict[str, str] = None
-    verify: str = None
+    auth: dict = None  # for login_captcha and login_email
+    cookies: dict = None  # for request auth
+    ssecurity: bytes = None  # for request encryption
+
     devices: list[dict] = None
 
     def __init__(self, session: ClientSession, servers: list = None):
@@ -54,63 +36,194 @@ class MiCloud:
         self.servers = servers or ["cn"]
         self.device_id = get_random_string(16)
 
-    async def login(self, username: str, password: str) -> bool:
+    @property
+    def ok(self):
+        return self.cookies is not None and self.ssecurity is not None
+
+    async def login(self, username: str, password: str, **kwargs) -> AuthResult:
         try:
-            payload = await self._login_step1()
-            data = await self._login_step2(username, password, payload)
-            self.verify = data.get("notificationUrl")
-            if not data["location"]:
-                return False
+            res1 = await self._service_login(username, password, **kwargs)
 
-            token = await self._login_step3(data["location"])
+            if captcha_url := res1.get("captchaUrl"):
+                data = await self._get_captcha_url(captcha_url)
+                self.auth = {
+                    "username": username,
+                    "password": password,
+                    "ick": data["ick"],
+                }
+                return {"ok": False, "captcha": data["image"]}
 
-            self.auth = {
-                "user_id": data["userId"],
-                "ssecurity": data["ssecurity"],
-                "service_token": token,
-            }
+            if notification_url := res1.get("notificationUrl"):
+                data = await self._send_email_ticket(notification_url)
+                self.auth = {"identity_session": data["identity_session"]}
+                return {"ok": False, "email": data["email"]}
 
-            return True
+            res2 = await self._get_location(res1["location"])
+
+            self.ssecurity = base64.b64decode(res1["ssecurity"])
+            self.cookies = {k: res2[k] for k in COOKIES}
+
+            return {"ok": True, "token": f"{res1['userId']}:{res1['passToken']}"}
 
         except Exception as e:
-            _LOGGER.exception(f"Can't login to Mi Cloud: {e}")
-            return False
+            return {"ok": False, "exception": e}
 
-    async def _login_step1(self) -> dict:
+    async def login_token(self, token: str) -> AuthResult:
+        try:
+            user_id, pass_token = token.split(":", 1)
+
+            r = await self.session.get(
+                "https://account.xiaomi.com/pass/serviceLogin",
+                cookies={"userId": user_id, "passToken": pass_token},
+                params={"_json": "true", "sid": SID},
+            )
+            res1 = parse_auth_response(await r.read())
+
+            res2 = await self._get_location(res1["location"])
+
+            self.ssecurity = base64.b64decode(res1["ssecurity"])
+            self.cookies = {k: res2[k] for k in COOKIES}
+
+            return {"ok": True}
+
+        except Exception as e:
+            return {"ok": False, "exception": e}
+
+    async def login_captcha(self, code: str) -> AuthResult:
+        return await self.login(
+            self.auth["username"], self.auth["password"], captcha_code=code
+        )
+
+    async def verify_email(self, ticket: str) -> AuthResult:
+        r = await self.session.post(
+            "https://account.xiaomi.com/identity/auth/verifyEmail",
+            cookies={"identity_session": self.auth["identity_session"]},
+            params={"_flag": 8, "ticket": ticket, "trust": "false", "_json": "true"},
+        )
+        res1 = parse_auth_response(await r.read())
+        assert res1["code"] == 0, res1
+
+        res2 = await self._get_location(res1["location"])
+
+        self.ssecurity = base64.b64decode(res2["ssecurity"])
+        self.cookies = {k: res2[k] for k in COOKIES}
+
+        return {"ok": True, "token": f"{res2['userId']}:{res2['passToken']}"}
+
+    async def _service_login(
+        self, username: str, password: str, captcha_code: str = None
+    ) -> dict:
         r = await self.session.get(
             "https://account.xiaomi.com/pass/serviceLogin",
-            cookies={"sdkVersion": "3.8.6", "deviceId": self.device_id},
-            headers={"User-Agent": UA % self.device_id},
-            params={"sid": "xiaomiio", "_json": "true"},
+            cookies={"sdkVersion": SDK_VERSION, "deviceId": self.device_id},
+            params={"_json": "true", "sid": SID},
         )
-        raw = await r.read()
-        _LOGGER.debug(f"MiCloud step1")
-        resp: dict = json.loads(raw[11:])
-        return {
-            k: v for k, v in resp.items() if k in ("sid", "qs", "callback", "_sign")
+        res1 = parse_auth_response(await r.read())
+
+        cookies = {"sdkVersion": SDK_VERSION, "deviceId": self.device_id}
+        data = {
+            "_json": "true",
+            "sid": res1["sid"],
+            "callback": res1["callback"],
+            "_sign": res1["_sign"],
+            "qs": res1["qs"],
+            "user": username,
+            "hash": hashlib.md5(password.encode()).hexdigest().upper(),
         }
 
-    async def _login_step2(self, username: str, password: str, payload: dict) -> dict:
-        payload["user"] = username
-        payload["hash"] = hashlib.md5(password.encode()).hexdigest().upper()
+        if captcha_code:
+            cookies["ick"] = self.auth["ick"]
+            data["captCode"] = captcha_code
 
         r = await self.session.post(
             "https://account.xiaomi.com/pass/serviceLoginAuth2",
-            cookies={"sdkVersion": "3.8.6", "deviceId": self.device_id},
-            data=payload,
-            headers={"User-Agent": UA % self.device_id},
-            params={"_json": "true"},
+            cookies=cookies,
+            data=data,
         )
-        raw = await r.read()
-        _LOGGER.debug(f"MiCloud step2")
-        resp = json.loads(raw[11:])
-        return resp
+        return parse_auth_response(await r.read())
 
-    async def _login_step3(self, location) -> str:
-        r = await self.session.get(location, headers={"User-Agent": UA})
-        service_token = r.cookies["serviceToken"].value
-        _LOGGER.debug(f"MiCloud step3")
-        return service_token
+    async def _get_location(self, location: str) -> dict:
+        r1 = await self.session.get(location)
+        assert await r1.read() == b"ok"
+
+        # this is useful for all steps
+        response = {k: v.value for k, v in r1.cookies.items()}
+
+        # this is useful for verify_email step
+        for r2 in r1.history:
+            response.update({k: v.value for k, v in r2.cookies.items()})
+            if ext := r2.headers.get("extension-pragma"):
+                response.update(json.loads(ext))
+
+        return response
+
+    async def _get_captcha_url(self, captcha_url: str) -> dict:
+        r = await self.session.get("https://account.xiaomi.com" + captcha_url)
+        body = await r.read()
+        return {"image": body, "ick": r.cookies["ick"]}
+
+    async def _send_email_ticket(self, notification_url: str) -> dict:
+        assert "/identity/authStart" in notification_url, notification_url
+        notification_url = notification_url.replace("authStart", "list")
+
+        r = await self.session.get(notification_url)
+        res1 = parse_auth_response(await r.read())
+        assert res1["code"] == 2, res1
+
+        identity_session = r.cookies["identity_session"]
+
+        r = await self.session.get(
+            "https://account.xiaomi.com/identity/auth/verifyEmail",
+            cookies={"identity_session": identity_session},
+            params={"_flag": 8, "_json": "true"},
+        )
+        res2 = parse_auth_response(await r.read())
+        assert res2["code"] == 0, res2
+
+        r = await self.session.post(
+            "https://account.xiaomi.com/identity/auth/sendEmailTicket",
+            cookies={"identity_session": identity_session},
+            data={"retry": 0, "icode": "", "_json": "true"},
+        )
+        res3 = parse_auth_response(await r.read())
+        assert res3["code"] == 0, res3
+
+        return {"email": res2["maskedEmail"], "identity_session": identity_session}
+
+    async def request(self, server: str, path: str, params: dict) -> dict:
+        form: dict[str, str] = {"data": json.dumps(params, separators=(",", ":"))}
+
+        nonce: bytes = gen_nonce()
+        signed_nonce: bytes = gen_signed_nonce(self.ssecurity, nonce)
+
+        # 1. gen hash for data param
+        form["rc4_hash__"] = gen_signature_base64(path, form, signed_nonce)
+
+        # 2. encrypt data and hash params
+        for k, v in form.items():
+            ciphertext: bytes = crypt(signed_nonce, v.encode())
+            form[k] = base64.b64encode(ciphertext).decode()
+
+        # 3. add signature for encrypted data and hash params
+        form["signature"] = gen_signature_base64(path, form, signed_nonce)
+
+        # 4. add nonce
+        form["_nonce"] = base64.b64encode(nonce).decode()
+
+        dom = "" if server == "cn" else server + "."
+
+        r = await self.session.post(
+            f"https://{dom}api.io.mi.com/app{path}", cookies=self.cookies, data=form
+        )
+        assert r.ok, r.status
+
+        ciphertext: bytes = base64.b64decode(await r.read())
+        plaintext: bytes = crypt(signed_nonce, ciphertext)
+
+        res = json.loads(plaintext)
+        assert res["code"] == 0, res
+
+        return res["result"]
 
     async def get_devices(self) -> list[dict] | None:
         payload = {
@@ -125,7 +238,7 @@ class MiCloud:
             resp = await self.request(server, "/v2/home/device_list_page", payload)
             if resp is None:
                 return None
-            total += resp["list"]
+            total.extend(resp["list"])
         return total
 
     async def get_rooms(self) -> list[dict] | None:
@@ -148,135 +261,36 @@ class MiCloud:
                 return resp["beaconkey"]
         return None
 
-    async def request(self, server: str, url: str, payload: dict):
-        assert server in SERVERS, "Wrong server: " + server
-        baseurl = (
-            "https://api.io.mi.com/app"
-            if server == "cn"
-            else f"https://{server}.api.io.mi.com/app"
-        )
 
-        data = json.dumps(payload, separators=(",", ":"))
-
-        nonce = gen_nonce()
-        signed_nonce = gen_signed_nonce(self.auth["ssecurity"], nonce)
-
-        try:
-            params = {"data": data}
-            params["rc4_hash__"] = gen_signature(url, signed_nonce, params)
-            params = {k: encrypt_rc4(signed_nonce, v) for (k, v) in params.items()}
-            params.update(
-                {
-                    "signature": gen_signature(url, signed_nonce, params),
-                    "ssecurity": self.auth["ssecurity"],
-                    "_nonce": nonce,
-                }
-            )
-            r = await self.session.post(
-                baseurl + url,
-                cookies={
-                    "userId": self.auth["user_id"],
-                    "serviceToken": self.auth["service_token"],
-                    "locale": "en_US",
-                },
-                headers={
-                    "Accept-Encoding": "identity",
-                    "User-Agent": UA,
-                    "x-xiaomi-protocal-flag-cli": "PROTOCAL-HTTP2",
-                    "MIOT-ENCRYPT-ALGORITHM": "ENCRYPT-RC4",
-                },
-                data=params,
-                timeout=10,
-            )
-
-            resp = await r.text()
-            resp = json.loads(decrypt_rc4(signed_nonce, resp))
-            # _LOGGER.debug(f"Response from MIoT API {url}: {resp}")
-            assert resp["code"] == 0, resp
-            return resp["result"]
-
-        except asyncio.TimeoutError:
-            _LOGGER.error(f"Timeout while requesting MIoT api {url}")
-        except Exception as e:
-            _LOGGER.error(f"Can't request MIoT API {url}", exc_info=e)
-
-        return None
+def parse_auth_response(body: bytes) -> dict:
+    assert body.startswith(b"&&&START&&&")
+    return json.loads(body[11:])
 
 
-class RC4:
-    _idx = 0
-    _jdx = 0
-    _ksa: list
-
-    def __init__(self, pwd):
-        self.init_key(pwd)
-
-    def init_key(self, pwd):
-        cnt = len(pwd)
-        ksa = list(range(256))
-        j = 0
-        for i in range(256):
-            j = (j + ksa[i] + pwd[i % cnt]) & 255
-            ksa[i], ksa[j] = ksa[j], ksa[i]
-        self._ksa = ksa
-        self._idx = 0
-        self._jdx = 0
-        return self
-
-    def crypt(self, data):
-        if isinstance(data, str):
-            data = data.encode()
-        ksa = self._ksa
-        i = self._idx
-        j = self._jdx
-        out = []
-        for byt in data:
-            i = (i + 1) & 255
-            j = (j + ksa[i]) & 255
-            ksa[i], ksa[j] = ksa[j], ksa[i]
-            out.append(byt ^ ksa[(ksa[i] + ksa[j]) & 255])
-        self._idx = i
-        self._jdx = j
-        self._ksa = ksa
-        return bytearray(out)
-
-    def init1024(self):
-        self.crypt(bytes(1024))
-        return self
-
-
-def get_random_string(length: int):
+def get_random_string(length: int) -> str:
     seq = string.ascii_uppercase + string.digits
-    return "".join((random.choice(seq) for _ in range(length)))
+    return "".join(random.choice(seq) for _ in range(length))
 
 
-def gen_nonce() -> str:
-    """Time based nonce."""
-    nonce = os.urandom(8) + int(time.time() / 60).to_bytes(4, "big")
-    return base64.b64encode(nonce).decode()
+def gen_nonce() -> bytes:
+    return os.urandom(8) + int(time.time() / 60).to_bytes(4, "big")
 
 
-def gen_signed_nonce(ssecret: str, nonce: str) -> str:
-    """Nonce signed with ssecret."""
-    m = hashlib.sha256()
-    m.update(base64.b64decode(ssecret))
-    m.update(base64.b64decode(nonce))
-    return base64.b64encode(m.digest()).decode()
+def gen_signed_nonce(ssecurity: bytes, nonce: bytes) -> bytes:
+    return hashlib.sha256(ssecurity + nonce).digest()
 
 
-def gen_signature(url: str, signed_nonce: str, data: dict) -> str:
-    """Request signature based on url, signed_nonce, nonce and data."""
-    sign = "&".join(
-        (["POST", url] + [f"{k}={v}" for k, v in data.items()] + [signed_nonce])
-    )
-    signature = hashlib.sha1(sign.encode()).digest()
+def gen_signature_base64(path: str, data: dict, signed_nonce: bytes) -> str:
+    params = ["POST", path]
+    for k, v in data.items():
+        params.append(f"{k}={v}")
+    params.append(base64.b64encode(signed_nonce).decode())  # use b64 signed nonce
+    signature = "&".join(params)
+    signature = hashlib.sha1(signature.encode()).digest()
     return base64.b64encode(signature).decode()
 
 
-def encrypt_rc4(pwd, data):
-    s = RC4(base64.b64decode(pwd.encode())).init1024().crypt(data.encode())
-    return base64.b64encode(s).decode()
-
-
-def decrypt_rc4(pwd, data):
-    return RC4(base64.b64decode(pwd.encode())).init1024().crypt(base64.b64decode(data))
+def crypt(key: bytes, data: bytes) -> bytes:
+    cipher = ARC4.new(key)
+    cipher.encrypt(bytes(1024))
+    return cipher.encrypt(data)
