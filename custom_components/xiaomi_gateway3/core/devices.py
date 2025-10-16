9886a421d8acca21b9a2bf877968dea0087a0dac
w101723 <taobaowang526@gmail.com>
2025-10-16 11:13:49 +0800

    fix giot.plug.v8icm voltage and power_consumption units

--- custom_components/xiaomi_gateway3/core/devices.py
+++ custom_components/xiaomi_gateway3/core/devices.py
@@ -5085,9 +5085,9 @@ DEVICES += [{
     21478: ["GranwinIoT", "Gra Electricity Metering Controller V8 (Mesh)", "giot.plug.v8icm"],
     "spec": [
         BaseConv("switch", "switch", mi="2.p.1"),
-        MathConv("power_consumption", "sensor", mi="4.p.1", multiply=0.001, round=3, entity={"units": "kWh"}),
+        MathConv("power_consumption", "sensor", mi="4.p.1", multiply=0.0001, round=3, entity={"units": "kWh"}),
         MathConv("electric_power", "sensor", mi="4.p.2", round=3, entity={"units": "W"}),
-        MathConv("voltage", "sensor", mi="4.p.3", multiply=0.001, round=2, entity={"units": "V"}),
+        MathConv("voltage", "sensor", mi="4.p.3", entity={"units": "V"}),
         MathConv("electric_current", "sensor", mi="4.p.4", multiply=0.001, round=3, entity={"units": "A"}),
         # BaseConv("auxiliary_switch", "switch", mi="7.p.1"),
         # BaseConv("voice_disable", "switch", mi="7.p.2"),
