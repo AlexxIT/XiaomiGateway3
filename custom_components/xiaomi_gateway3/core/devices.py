c1166343a237f3bec5b68043fce6d0cc0d405fe6
kei81131 <111072203+kei81131@users.noreply.github.com>
2026-01-09 14:55:14 +0800

    Update device properties for has_someone_duration and no_one_duration

--- custom_components/xiaomi_gateway3/core/devices.py
+++ custom_components/xiaomi_gateway3/core/devices.py
@@ -1801,8 +1801,8 @@ DEVICES += [{
         BaseConv("battery", "sensor", mi="7.p.1003", entity=ENTITY_LAZY),  # uint8
         BoolConv("customized_property", "binary_sensor", mi="5.p.1019"), #custom close range
         MapConv("event_type", "sensor", mi="5.e.1097.p.1", map={0: "0_Reset", 1: "1_OffLightLeave", 2: "2_OffLightExit", 3: "3_OffLightSleep", 4: "4_OffLightExit", 5: "5_RadarNoiseExceedMinor", 6: "6_RadarNoiseExceedCritical", 7: "7_RadarNoiseNormal", 8: "8_RadarNoiseError"}),
-        BaseConv("has_someone_duration", "sensor", mi="2.p.1080", entity={"enabled": False, "units": UNIT_MINUTES}),  # uint8
-        BaseConv("no_one_duration", "sensor", mi="2.p.1079", entity={"enabled": False, "units": UNIT_MINUTES})  # uint8
+        BaseConv("has_someone_duration", "sensor", mi="2.p.1081", entity={"enabled": False, "units": UNIT_MINUTES}),  # uint8
+        BaseConv("no_one_duration", "sensor", mi="2.p.1082", entity={"enabled": False, "units": UNIT_MINUTES})  # uint8
     ],
 }, {
     8613: ["H+", "Double Wall Switch", "huca.switch.dh2"],
